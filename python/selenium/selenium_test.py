dd3f8e615d00d403a0281d3e343a4377c2ce4f58
Kenneth <edowon0623@gmail.com>
2021-01-19 17:53:30 +0900

    upload scrapy files

--- python/selenium/selenium_test.py
+++ python/selenium/selenium_test.py
@@ -3,4 +3,9 @@ from selenium import webdriver
 path = "C:\\Users\\i\\Work\\webdriver\\chromedriver.exe"
 driver = webdriver.Chrome(path)
 
-driver.get("https://www.naver.com")
+driver.get("https://www.google.com")
+print(driver.title)
+
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("아마존 웹 서비스")
+search_box.submit()
